6b36c9e63823bd73c53bb7a8e1612159d226ce2f
DESKTOP-QPGUH2T\sv <jhmin@sharevaluecorp.com>
2022-06-08 13:41:50 +0900

    install axios

--- pages/main.vue
+++ pages/main.vue
@@ -1,19 +1,55 @@
 <template>
   <div>
-    <Logo/>
+    <p>첫 번째 carts: {{ loadData }}</p>
+    <Logo :loadData="loadData"/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Logo from '@/components/Logo.vue'
+import axios from 'axios'
 
 export default Vue.extend({
   name: '',
+  // 방법1 적용 시, 아래 사용 한다. 방법2 적용 시, server 에서 data 를 처리하고 보내주므로, data 설정할 필요 없다!
+  // data(){
+  //   return {
+  //     loadData: [],
+  //   }
+  // },
   components: {
     Logo
   },
+  // 방법1. created axios 요청
+  // async created() {
+  //   const response = await axios.get('http://localhost:3000/carts')  // server url
+  //   // console.log('res: ', response);
+  //   this.loadData = response.data;
+  // }
+
+  // 방법2. asyncData axios 요청
+  async asyncData(){
+    const response = await axios.get('http://localhost:3000/carts')  // server url, https 아니다.
+    console.log(response.data[0]);
+    const loadData = response.data;
+    return { loadData: loadData }  // 객체 축약 가능
+  }
 })
 </script>
 
 <style scoped></style>
+<!-- 목표:
+1. [] main.vue 생성 후, 즉시 데이터를 가져오기
+- axios 와 created 함수 사용하여 처리하면 될 듯
+2. async await 처리 전 후 차이점 이해 하는지?
+- 처리 전: promise <pending> 상태로 나타내고
+- 처리 후: 객체로 나타낸다.
+3. created() 와 asyncData() 의 차이점 아는지?
+- created() 사용 시, 깜빡임 현상 발생한다.
+- FIXME: asyncData() 사용하면 깜빡임 없는지??
+4. asyncData 는 page 폴더 아래에서만 사용 가능하다.
+- component 에서 사용 불가능
+- component 에서 사용하기 위해서는 데이터를 내려준다.
+5.
+-->
